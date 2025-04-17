ada400963267bb82d159610463a5ed93d6755170
AstroX11 <devastro0010@gmail.com>
2025-04-17 23:10:58 +0100

    fix

--- src/messaging/Services/m-upsert.ts
+++ src/messaging/Services/m-upsert.ts
@@ -2,7 +2,7 @@ import Message from '../Messages/Message.ts';
 import { log } from '../../utils/index.ts';
 import { messageDb } from '../../models/index.ts';
 import { serialize } from '../serialize.ts';
-import { runCommand } from '../command.ts';
+import { commands } from '../plugins.ts';
 import type { BaileysEventMap, WASocket } from 'baileys';
 
 export default class MessageUpsert {
@@ -26,10 +26,41 @@ export default class MessageUpsert {
       message,
      }),
 
-     await runCommand(cmdInstance),
+     await this.runCommand(cmdInstance),
     ]);
    } catch (error) {
     log.error(`Task error: ${error}`);
+   }
+  }
+ }
+
+ async runCommand(message: Message) {
+  if (!message.data.text) return;
+
+  for (const cmd of commands) {
+   const handler = message.data.prefix.find((p: string) =>
+    message.data.text?.startsWith(p),
+   );
+   const match = message.data.text
+    .slice(handler?.length || 0)
+    .match(cmd.name as string);
+   if (!handler || !match) continue;
+
+   if (message.mode && !message.sudo) continue;
+   if (cmd.fromMe && !message.sudo) {
+    await message.send('_This command is for Sudo Only!_');
+    continue;
+   }
+   if (cmd.isGroup && !message.isGroup) {
+    await message.send('_This command is for Groups Only!_');
+    continue;
+   }
+
+   try {
+    await message.react('⏳');
+    await cmd.function(message, match[2] ?? '');
+   } catch (err) {
+    log.error(err);
    }
   }
  }
