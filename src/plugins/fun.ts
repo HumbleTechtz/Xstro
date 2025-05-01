aaf22b70898dd2f5b2cea8601e6cc2a2b68a72be
AstroX11 <devastro0010@gmail.com>
2025-05-01 13:30:58 +0100

    Update fun.ts

--- src/plugins/fun.ts
+++ src/plugins/fun.ts
@@ -5,11 +5,24 @@ Command({
  name: 'rizz',
  fromMe: false,
  isGroup: false,
+ desc: 'Rizz your babe lol',
  type: 'fun',
  function: async (msg) => {
   const data = JSON.parse(await fetch('https://rizzapi.vercel.app/random')) as {
    text: string;
   };
   return await msg.send(data.text);
+ },
+});
+
+Command({
+ name: 'insult',
+ fromMe: false,
+ isGroup: false,
+ desc: 'Insult one',
+ type: 'fun',
+ function: async (msg) => {
+  const insult = await fetch('https://insult.mattbas.org/api/insult');
+  return await msg.send(insult);
  },
 });
