18dfcae2056f30e4fcfc2b68fb1fc09ba695c3c8
AstroX11 <devastro0010@gmail.com>
2025-05-02 12:23:24 +0100

    Update settings.ts

--- src/plugins/settings.ts
+++ src/plugins/settings.ts
@@ -1,5 +1,5 @@
 import { Command } from '../messaging/plugins.ts';
-import { setSettings } from '../models/settings.ts';
+import { getSettings, setSettings } from '../models/settings.ts';
 
 Command({
  name: 'setprefix',
@@ -17,5 +17,33 @@ Command({
   return await msgs.send(
    `_Bot Prefix updated to "${args}"_\nUsage: ${args}ping`,
   );
+ },
+});
+
+Command({
+ name: 'mode',
+ fromMe: true,
+ isGroup: false,
+ desc: 'Set bot mode to be public or private',
+ type: 'settings',
+ function: async (message, match) => {
+  if (!match || !['private', 'public'].includes(match.toLowerCase())) {
+   return message.send(
+    `*Usage:*\n${message.prefix[0]}mode private\n${message.prefix[0]}mode public`,
+   );
+  }
+
+  const settings = (await getSettings()).mode;
+
+  if (settings && match.toLowerCase() === 'private') {
+   return await message.send('_Already in Private Mode_');
+  } else if (!settings && match.toLowerCase() === 'public') {
+   return await message.send('_Already in Public Mode_');
+  }
+
+  const value = match.toLowerCase() === 'private' ? 1 : 0;
+  await setSettings('mode', value);
+
+  return await message.send(`_Bot is now in ${match.toLowerCase()} mode_`);
  },
 });
