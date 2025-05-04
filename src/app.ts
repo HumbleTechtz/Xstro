76471123cba4e53d9c1f9948abb926e571445ad9
AstroX11 <devastro0010@gmail.com>
2025-05-04 20:23:31 +0100

    lint and add server port

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,19 @@
+import http from 'http';
 import WhatsAppClient from './messaging/client.ts';
 import { syncPlugins } from './messaging/plugins.ts';
 import { print } from './utils/constants.ts';
 import { disablelogs } from './utils/console.js';
+
+const PORT = 8000;
+
+http
+ .createServer((req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  res.end('Alive.\n');
+ })
+ .listen(PORT, () => {
+  print.succeed(`Connected to PORT: ${PORT}`);
+ });
 
 export default class App {
  constructor() {
