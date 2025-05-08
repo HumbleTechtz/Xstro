c7500bcca78effda92009f58a4372390332b4558
AstroX11 <devastro0010@gmail.com>
2025-05-08 11:19:33 +0100

    Update app.ts

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,12 @@
 import client from './messaging/client.ts';
 import { syncPlugins } from './messaging/plugins.ts';
-import { print } from './utils/constants.ts';
 
 export async function startApp() {
  try {
   await syncPlugins('../plugins', ['.ts']);
   await client();
  } catch (error) {
-  print.fail(error as string);
+  console.log(error);
  }
 }
 
