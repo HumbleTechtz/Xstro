b65e450acd84ff09b18c28ffc9f2ae60a8b4831e
AstroX11 <211725231+AstroX11@users.noreply.github.com>
2025-05-27 12:05:56 +0100

    fix update message

--- src/plugins/updater.ts
+++ src/plugins/updater.ts
@@ -1,9 +1,5 @@
-import { exec } from 'child_process';
-import { promisify } from 'util';
 import { Command } from '../messaging/plugin.ts';
-import { delay } from 'baileys';
-
-const execPromise = promisify(exec);
+import { update } from '../utils/updater.ts';
 
 Command({
 	name: 'update',
@@ -13,50 +9,36 @@ Command({
 	type: 'utilities',
 	function: async (message, match) => {
 		const prefix = message.prefix[0];
+		const result = await update(match === 'now');
 
-		try {
-			await execPromise('git fetch');
-			const { stdout: logOutput } = await execPromise(
-				'git log stable..origin/stable',
-			);
-			const commits = logOutput.trim().split('\n').filter(Boolean);
-
-			if (match === 'now') {
-				if (commits.length === 0) {
-					return await message.send(
-						'```You are already on the latest version```',
-					);
-				}
-
-				await message.send('Updating...');
-				await execPromise('git stash');
-				await execPromise('git pull origin stable');
-
-				await message.send('Installing new dependencies...');
-				const installation = await execPromise('pnpm install');
-				await message.send(
-					`\`\`\`'Dependencies updated'\n${installation.stdout}\`\`\``,
-				);
-				await delay(2000);
-				await message.send('Restarting...');
-				process.exit(0);
-			} else {
-				if (commits.length === 0) {
-					return await message.send(
-						'```You are already on the latest version```',
-					);
-				}
+		switch (result.status) {
+			case 'up-to-date':
+				await message.send('```You are on the latest version```');
+				break;
+			case 'updates-available':
+				const commitMessages = result
+					?.commits!.map(commit => {
+						const lines = commit.split('\n');
+						const messageLine = lines.find(line => line.startsWith('    '));
+						return messageLine ? messageLine.trim() : '';
+					})
+					.filter(Boolean);
 
 				let changes = 'New update available\n\n';
-				changes += `Commits: ${commits.length}\nChanges:\n`;
-				commits.forEach((commit, index) => {
-					changes += `${index + 1}. ${commit}\n`;
+				changes += `Fixes: ${commitMessages.length}\nDetails:\n`;
+				commitMessages.forEach((message, index) => {
+					changes += `${index + 1}. ${message}\n`;
 				});
 				changes += `\nUse, ${prefix}update now`;
 				await message.send(changes);
-			}
-		} catch (err) {
-			await message.send('❌ Update failed:\n```' + err + '```');
+				break;
+			case 'updated':
+				await message.send('Updating...');
+				process.exit(0);
+				break;
+			case 'error':
+				await message.send('Update failed:\n```' + result.error + '```');
+				break;
 		}
 	},
 });
