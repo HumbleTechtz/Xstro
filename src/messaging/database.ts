be3e97281c1ac6246101e1cb4a74e81b62c46e42
AstroX11 <211725231+AstroX11@users.noreply.github.com>
2025-05-27 14:57:16 +0100

    Update database.ts

--- src/messaging/database.ts
+++ src/messaging/database.ts
@@ -1,25 +1,6 @@
 import { Database, JournalMode } from 'quantava';
 
-const database = new Database('database.db', { journalMode: JournalMode.WAL });
-
-process.on('exit', () => {
-	database.close();
+let database: Database;
+export default database = new Database('database.db', {
+	journalMode: JournalMode.WAL,
 });
-
-process.on('SIGINT', () => {
-	database.close();
-});
-
-process.on('SIGTERM', () => {
-	database.close();
-});
-
-process.on('uncaughtException', err => {
-	console.log('Error:', err);
-});
-
-process.on('unhandledRejection', er => {
-	console.log('Error', er);
-});
-
-export default database;
