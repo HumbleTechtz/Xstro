493eb95b2d668cb15a4cfdda318845f858a6951b
AstroX11 <211725231+AstroX11@users.noreply.github.com>
2025-05-29 06:41:19 +0100

    upgrade dependencies

--- src/messaging/client.ts
+++ src/messaging/client.ts
@@ -11,6 +11,7 @@ import config from "../../config.mjs";
 import events from "./events.ts";
 import hooks from "./hooks.ts";
 import useSqliteAuthState from "../utils/useSqliteAuthState.ts";
+import { pairClient } from "./pair.ts";
 import { getMessage, cachedGroupMetadata } from "../models/index.ts";
 
 (async () => {
@@ -39,24 +40,7 @@ import { getMessage, cachedGroupMetadata } from "../models/index.ts";
 		cachedGroupMetadata,
 	});
 
-	if (!sock.authState?.creds?.registered) {
-		const phoneNumber = config.USER_NUMBER?.replace(/[^0-9]/g, "") ?? "";
-		if (phoneNumber.length < 11) {
-			console.error("Please input a valid number");
-			process.exit(1);
-		}
-		await new Promise(resolve => setTimeout(resolve, 2000));
-		console.log(`Pairing Code: ${await sock.requestPairingCode(phoneNumber)}`);
-
-		await new Promise<void>(resolve => {
-			const isRegistered = setInterval(() => {
-				if (sock.authState?.creds?.registered) {
-					clearInterval(isRegistered);
-					resolve();
-				}
-			}, 1000);
-		});
-	}
+	await pairClient(sock);
 
 	await Promise.all([events(sock, { saveCreds }), hooks(sock)]);
 
