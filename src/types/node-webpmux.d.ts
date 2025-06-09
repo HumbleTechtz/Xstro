5316527a77fbc22343929124efa1b775a915d0eb
DevAstro <devastro0010@gmail.com>
2025-06-09 18:47:54 +0100

    fix

--- src/types/node-webpmux.d.ts
+++
@@ -1,8 +0,0 @@
-declare module "node-webpmux" {
-	export class Image {
-		exif: Buffer;
-		constructor();
-		load(filePath: string | Buffer): Promise<void>;
-		save(outputPath?: string): Promise<Buffer>;
-	}
-}
