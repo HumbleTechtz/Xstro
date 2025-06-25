28de22724319327f4d7f0167e662cbf4fd915bf3
DevAstro <devastro0010@gmail.com>
2025-06-25 08:10:58 +0100

    Ignore stderr output in client process spawn
    Changed the stdio option in the spawn call to ignore stderr output for the client process. This reduces noise from error logs in the console.

--- app.ts
+++ app.ts
@@ -3,7 +3,7 @@ import database from "./client/Core/database";
 
 const startClient = () => {
 	spawn("bun", ["run", "./client/Core/client"], {
-		stdio: "inherit",
+		stdio: ["inherit", "inherit", "ignore"],
 	}).on("exit", code =>
 		code === 0 ? startClient() : (database.close(), process.exit(code))
 	);
